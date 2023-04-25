2573b5d2febf73134b22fd0205925733f994506a
Yaroslav <sych521@gmail.com>
2023-04-25 16:13:42 +0300

    in process

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries.js';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
@@ -15,10 +16,64 @@ function clearMarkup() {
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(event) {
-  const countryToFind = event.target.value.trim().toLowerCase();
+  const countryToFind = event.target.value.trim();
   if (!countryToFind) {
     clearMarkup();
     return;
   }
-  fetchCountries(countryToFind);
+  fetchCountries(countryToFind)
+    .then(checkResponse)
+    .then(checkCountriesNumber)
+    .catch(onError);
+
+  function checkResponse(response) {
+    clearMarkup();
+    if (!response.ok) {
+      return console.error('Response is NOT OK!');
+    }
+    console.log(response.json()); // DELETE WHEN DONE
+    return response.json();
+  }
+}
+
+// CHECKS
+function checkCountriesNumber(data) {
+  console.log(YYYYYYYYYYYYYYYYYYEEEEEEEEEEEEEEEEEEESSSSSSSSSSSSSSSS);
+  if (data.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.🤔'
+    );
+    return;
+  } else if (data.length >= 2) {
+    const markup = data
+      .map(element => {
+        return buildListMarkup(element.name, element.flag);
+      })
+      .join('');
+    return (countryList.innerHTML = markup);
+  }
+}
+
+function onError() {
+  Notiflix.Notify.info('Oops, there is no country with that name 😞');
+}
+
+// BUILD COUNTRY LIST MARKUP
+function buildCoutryListMarkup(name, flag) {
+  return `
+	<li class="listItem">
+	<img src="${flag}" alt="flag of ${name}" class="flag" />
+	<p class="country-name">${name}</p>
+	</li>
+	`;
+}
+
+// BUILD ONE COUNTRY MARKUP
+function buildCountryMarkup(name, flag) {
+  return `
+	<li class="listItem">
+	<img src="${flag.svg}" alt="$flag of ${name.official}" class="flag" />
+	<p class="country-name">${name.official}</p>
+	</li>
+	`;
 }
